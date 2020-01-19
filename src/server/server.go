7259d4d87cfe83d4689d43784b420b9aaaea62f7
Alban PRATS <alban.prats@gmail.com>
2020-01-19 11:34:40 +0100

    quick update

--- src/server/server.go
+++ src/server/server.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
-	"math"
-	"regexp"
 )
 
-type void struct {}
+type void struct{}
 
-type ack struct{
-	n string
+type ack struct {
+	n   string
 	toa int64
 }
 
 type waitParam struct {
 	toa int64
-	c chan int64
+	c   chan int64
 }
 
 type ack_list struct {
@@ -32,49 +32,48 @@ type ack_list struct {
 	value string
 }
 
-
 var (
-	lock sync.Mutex
+	lock     sync.Mutex
 	portList map[int]void
-	m void
+	m        void
 )
 
-const debug = false
+var debug = false
 
 const BufferSize = 1494
-
 const attenuation_coefficient float32 = 0.5
 const incrementation_ca = 1
 
 // Potential RTT/RTO/SRTT evolution
 const R = 0.0
+
 // real initization will be done after first handshake (SYN-ACK -> ACK) will be the R variable (in ms)
 
-const alpha = 1/8 // (RFC6298)
-const beta = 1/4 // (RFC6298)
-const K = 8 // (RFC6298)
+const alpha = 1 / 8 // (RFC6298)
+const beta = 1 / 4  // (RFC6298)
+const K = 8         // (RFC6298)
 
 type conn_param struct {
-	SRTT float64
-	RTTVAR float64
-	RTO float64
-	cwnd int
+	SRTT            float64
+	RTTVAR          float64
+	RTO             float64
+	cwnd            int
 	congestion_type string
 }
 
-func NewConn_param (r float64) conn_param {
+func NewConn_param(r float64) conn_param {
 	cp := conn_param{
-		SRTT: r,
-		RTTVAR: r/2,
-		RTO: r + K * r/2,
-		cwnd: 1,
+		SRTT:            r,
+		RTTVAR:          r / 2,
+		RTO:             r + K*r/2,
+		cwnd:            1,
 		congestion_type: "SS",
 	}
 	return cp
 }
 
 func logs(v ...interface{}) {
-	if (debug == true){
+	if debug == true {
 		fmt.Println(v)
 	}
 }
@@ -82,7 +81,7 @@ func logs(v ...interface{}) {
 func getPort() int {
 	lock.Lock()
 	defer lock.Unlock()
-	for k := range portList{
+	for k := range portList {
 		delete(portList, k)
 		return k
 	}
@@ -102,17 +101,21 @@ func testPort(p int) int {
 	if err == nil {
 		pc.Close()
 		return p
-	}else{
+	} else {
 		logs("erreur avec le port " + port)
 		return -1
 	}
 }
 
 func testPorts(portMin int, portMax int) {
-	for p:=portMin; p<=portMax; p++ {
+	for p := portMin; p <= portMax; p++ {
 		releasePort(testPort(p))
 	}
 }
+
+//const alpha = 1/8 // (RFC6298)
+//const beta = 1/4 // (RFC6298)
+//const K = 8 // (RFC6298)
 
 func update_time_mesure(new_measure float64, cp *conn_param) {
 	cp.SRTT = (1-alpha)*cp.SRTT + alpha*new_measure
@@ -120,8 +123,7 @@ func update_time_mesure(new_measure float64, cp *conn_param) {
 	cp.RTO = cp.SRTT + K*cp.RTTVAR
 }
 
-
-func cwnd_evolution (flag int, seq_failed int, cp *conn_param){
+func cwnd_evolution(flag int, seq_failed int, cp *conn_param) {
 	/*
 		Function that will deal with the evolution of our congestion window and
 		that will handle the switch from slow start to congestion avoidance and
@@ -136,39 +138,39 @@ func cwnd_evolution (flag int, seq_failed int, cp *conn_param){
 	*/
 	logs("Evolution of cwnd")
 	switch flag {
-		case 0:
-			switch (cp.congestion_type){
-				case "SS":
-					logs("SS WINDOW")
-					cp.cwnd *= 2
+	case 0:
+		switch cp.congestion_type {
+		case "SS":
+			logs("SS WINDOW")
+			cp.cwnd *= 2
 
-				case "CA":
-					logs("In CA")
-					cp.cwnd += incrementation_ca
-			}
-		case 1:
-			switch (cp.congestion_type){
-					case "SS":
-						if (seq_failed > 0){
-							  logs("To CA")
-								cp.cwnd = int(float32(cp.cwnd)*attenuation_coefficient)+1 //index ?
-								cp.congestion_type="CA"
-							}				// case timeout to handle
+		case "CA":
+			logs("In CA")
+			cp.cwnd += incrementation_ca
+		}
+	case 1:
+		switch cp.congestion_type {
+		case "SS":
+			if seq_failed > 0 {
+				logs("To CA")
+				cp.cwnd = int(float32(cp.cwnd)*attenuation_coefficient) + 1 //index ?
+				cp.congestion_type = "CA"
+			} // case timeout to handle
 
-					case "CA":
-						cp.cwnd = int(float32(cp.cwnd)*attenuation_coefficient)+1
-				}
+		case "CA":
+			cp.cwnd = int(float32(cp.cwnd)*attenuation_coefficient) + 1
+		}
 
 	}
 }
 
 // https://kgrz.io/reading-files-in-go-an-overview.html
-func readFile(file string) ([][]byte, int, int64){
+func readFile(file string) ([][]byte, int, int64) {
 	//file, _ = regexp.MatchString()
 	//file = "coucou "
 	absFile, _ := filepath.Abs(file)
 	f, err := os.Open(absFile)
-	if err != nil{
+	if err != nil {
 		logs("Error while openning", absFile)
 		return nil, 0, 0
 	}
@@ -183,7 +185,7 @@ func readFile(file string) ([][]byte, int, int64){
 		d := make([]byte, BufferSize)
 		n, _ = f.Read(d)
 		//logs("Nombre de bytes lus", n)
-		if n == 0{
+		if n == 0 {
 			break
 		}
 		m = n
@@ -200,14 +202,14 @@ func readFile(file string) ([][]byte, int, int64){
 	return data, m, f_i
 }
 
-func readpc(pc net.PacketConn, ch chan ack, logfile *string, timelog time.Time){
-	for{
+func readpc(pc net.PacketConn, ch chan ack, logfile *string, timelog time.Time) {
+	for {
 		buffer := make([]byte, 100)
-		n,_,_ := pc.ReadFrom(buffer)
-		if (n > 0){
-		ch <- ack{string(buffer[:n-1]),time.Now().UnixNano()}
-		*logfile += "r " + time.Now().Sub(timelog).String() + " " + string(buffer[3:n-1]) + " " + strconv.Itoa(n) + " \n"
-	}
+		n, _, _ := pc.ReadFrom(buffer)
+		if n > 0 {
+			ch <- ack{string(buffer[:n-1]), time.Now().UnixNano()}
+			*logfile += "r " + time.Now().Sub(timelog).String() + " " + string(buffer[3:n-1]) + " " + strconv.Itoa(n) + " \n"
+		}
 	}
 }
 
@@ -216,20 +218,20 @@ func readpc(pc net.PacketConn, ch chan ack, logfile *string, timelog time.Time){
 }
 */
 
-func contains_find(a []ack_list, x string) (bool,int) {
-				for i, n := range a {
-          if strings.Compare(x,n.value) == 0 {
-              return true,i
-          }
-        }
-        return false,0
+func contains_find(a []ack_list, x string) (bool, int) {
+	for i, n := range a {
+		if strings.Compare(x, n.value) == 0 {
+			return true, i
+		}
+	}
+	return false, 0
 }
 
 func sendFile(file string, pc net.PacketConn, add net.Addr, cp *conn_param) bool {
 	var last_ack = ""
-	var log_out = "" //logging will be like : time index buffer_size
+	var log_out = ""                            //logging will be like : time index buffer_size
 	data, last_len, file_info := readFile(file) // data : array of data size of buffer
-	logs("Data longeur ",len(data))
+	logs("Data longeur ", len(data))
 	seqn0, i := 000001, 0
 	ch := make(chan ack, 1000)
 	startlog := time.Now()
@@ -237,22 +239,22 @@ func sendFile(file string, pc net.PacketConn, add net.Addr, cp *conn_param) bool
 	var ack_array []ack_list // Initial array with all expected ACKs
 	var next_id = 0
 	backoff := 1 //backoff when timing out
-	for i < len(data){
+	for i < len(data) {
 		logs("Taille de la fenêtre ", *cp)
-		for j := 0; j < cp.cwnd; j++{
+		for j := 0; j < cp.cwnd; j++ {
 			//toSend := make([]byte, 1500)
 			bs := fmt.Sprintf("%06d", seqn0)
-			elt_list := ack_list{i,bs}
+			elt_list := ack_list{i, bs}
 			ack_array = append(ack_array, elt_list) // configure all elements to send + to send again
 
-			if (i == len(data)-1){ // si dermier packet à envoyer
+			if i == len(data)-1 { // si dermier packet à envoyer
 				data[i] = data[i][:last_len]
-				seqn0 ++
-				i ++
+				seqn0++
+				i++
 				break
 			}
-			seqn0 ++
-			i ++
+			seqn0++
+			i++
 		}
 
 		for _, elt := range ack_array { //for each in batch
@@ -262,111 +264,113 @@ func sendFile(file string, pc net.PacketConn, add net.Addr, cp *conn_param) bool
 			log_out += "e " + time.Now().Sub(startlog).String() + " " + elt.value + " " + strconv.Itoa(len(ack_array)) + " \n"
 			//TODO : trace when sendend to know when timeouts or we can evaluate each rtt
 
-				/*sbuffer := ""
-				select{
-				case <- time.After(1*time.Second):
-						sbuffer = "erreur"
-					case sbuffer = <- ch:
-				}
-				if(strings.Contains(sbuffer, bs)){
-					break
-				}
-				*/
+			/*sbuffer := ""
+			select{
+			case <- time.After(1*time.Second):
+					sbuffer = "erreur"
+				case sbuffer = <- ch:
 			}
+			if(strings.Contains(sbuffer, bs)){
+				break
+			}
+			*/
+		}
 
 		for { //Checking ACKs loop
 			exit := 0
-			if (len(ack_array) == 0){ //Ya R, what is done is done
+			if len(ack_array) == 0 { //Ya R, what is done is done
 				logs("All ACK expected were received")
-				cwnd_evolution(0,-1, cp)
+				cwnd_evolution(0, -1, cp)
 				break
 			}
-			select{
-				case ack_buffer, content := <- ch: // content false => buffer empty
+			select {
+			case ack_buffer, content := <-ch: // content false => buffer empty
 				backoff = 1 //resetting backoff value
-				 logs("Getting data from channel")
-				 logs("Content or no longer content ?", content)
-				 logs("Waiting from ACKs :")
-				 logs(ack_array)
-				 logs("Trading with ACK", ack_buffer.n[3:]) //ACK be like ACK000124 so [3:]
-				 exists, index := contains_find(ack_array, ack_buffer.n[3:]) // structure from channel (ack)
-				 logs("Last ACK sent was ", last_ack, "and this one ", ack_buffer.n)
-				 res_buffer,_ := strconv.Atoi(ack_buffer.n[3:])
-				 res_ackarray,_ := strconv.Atoi(ack_array[len(ack_array)-1].value)
-				 // logs(exists)
-					if (len(ack_array) != 0 && content == true &&  res_buffer > res_ackarray){
-						logs("Packet way beyond !")
-						i = res_buffer-1
-						seqn0 = i+1
-						ack_array = nil
-						cwnd_evolution(1, index, cp)
-						exit = 1
-						logs("Envoi du paquet pas réussi")
-						break
-					}
-					received := false
-					for ack_buffer.n == last_ack && !(received){
-						logs("Similar ACKs revoyer.")
-						last_id,_ := strconv.Atoi(ack_buffer.n[3:])
-						toSend := append([]byte(fmt.Sprintf("%06d", last_id+1)), data[last_id]...)
-						logs("Spot error about to send again...packet ", last_id+1)
-						pc.WriteTo(toSend, add)
-						log_out += "* " + time.Now().Sub(startlog).String() + " " + strconv.Itoa(last_id+1) + " 1 \n"
-						for {
-							fexit := false
-							select{
-							case ack_ans, content := <- ch:
-									logs(content)
-								  to_compare,_ := strconv.Atoi(ack_ans.n[3:])
-									if (to_compare != last_id){
-									next_id,_ = strconv.Atoi(ack_ans.n[3:])
-									logs("On recommence au paquet ",next_id+1)
-									i = next_id
-									seqn0 = i+1
-									fexit = true
-									ack_array = nil
-									received = true
-									last_ack = ack_buffer.n
-									logs("Value of i en sortie ",i)
-									cwnd_evolution(1, 1 ,cp) // Congestion avoidance
-									break}
-							default:
-								received = false
-								time.Sleep(time.Duration(int(cp.RTO)) * time.Nanosecond)
+				logs("Getting data from channel")
+				logs("Content or no longer content ?", content)
+				logs("Waiting from ACKs :")
+				logs(ack_array)
+				logs("Trading with ACK", ack_buffer.n[3:])                  //ACK be like ACK000124 so [3:]
+				exists, index := contains_find(ack_array, ack_buffer.n[3:]) // structure from channel (ack)
+				logs("Last ACK sent was ", last_ack, "and this one ", ack_buffer.n)
+				res_buffer, _ := strconv.Atoi(ack_buffer.n[3:])
+				res_ackarray, _ := strconv.Atoi(ack_array[len(ack_array)-1].value)
+				// logs(exists)
+				if len(ack_array) != 0 && content == true && res_buffer > res_ackarray {
+					logs("Packet way beyond !")
+					i = res_buffer - 1
+					seqn0 = i + 1
+					ack_array = nil
+					cwnd_evolution(1, index, cp)
+					exit = 1
+					logs("Envoi du paquet pas réussi")
+					break
+				}
+				received := false
+				for ack_buffer.n == last_ack && !(received) {
+					logs("Similar ACKs revoyer.")
+					last_id, _ := strconv.Atoi(ack_buffer.n[3:])
+					toSend := append([]byte(fmt.Sprintf("%06d", last_id+1)), data[last_id]...)
+					logs("Spot error about to send again...packet ", last_id+1)
+					pc.WriteTo(toSend, add)
+					log_out += "* " + time.Now().Sub(startlog).String() + " " + strconv.Itoa(last_id+1) + " 1 \n"
+					for {
+						fexit := false
+						select {
+						case ack_ans, content := <-ch:
+							logs(content)
+							to_compare, _ := strconv.Atoi(ack_ans.n[3:])
+							if to_compare != last_id {
+								next_id, _ = strconv.Atoi(ack_ans.n[3:])
+								logs("On recommence au paquet ", next_id+1)
+								i = next_id
+								seqn0 = i + 1
 								fexit = true
+								ack_array = nil
+								received = true
+								last_ack = ack_buffer.n
+								logs("Value of i en sortie ", i)
+								cwnd_evolution(1, 1, cp) // Congestion avoidance
 								break
+							}
+						default:
+							received = false
+							time.Sleep(time.Duration(int(cp.RTO)) * time.Nanosecond)
+							fexit = true
+							break
 
 							//time.sleep du RTT
 						}
-						if (fexit) {
+						if fexit {
 							break
 						}
 					}
-					}
-					if (exists && len(ack_array) > 0){
-						ack_array = ack_array[index+1:]
-						last_ack = ack_buffer.n
-					}
+				}
+				if exists && len(ack_array) > 0 {
+					ack_array = ack_array[index+1:]
+					last_ack = ack_buffer.n
+				}
 				//case <- time.After(math.Round(cp.RTO * time.Second): //First etch of timeout
-			case <- time.After( time.Duration(int(cp.RTO)) * time.Nanosecond):
-					logs("Timed out - backoff:", backoff)
-					cwnd_evolution(1, ack_array[0].index, cp)
-					cp.RTO = cp.RTO * math.Pow(float64(2), float64(backoff))
-					if (cp.RTO > 50000000){
-						cp.RTO = 1000000
-					}
-					backoff ++
-					i = ack_array[0].index
-					seqn0 = i + 1
-					ack_array = nil
-					exit = 1
-					fmt.Println(ack_array)
-					break
-			}
-			if (exit == 1){
+			case <-time.After(time.Duration(int(cp.RTO)) * time.Nanosecond):
+				logs("Timed out - backoff:", backoff)
+				cwnd_evolution(1, ack_array[0].index, cp)
+				cp.RTO = cp.RTO * math.Pow(float64(2), float64(backoff))
+				if cp.RTO > 50000000 {
+					cp.RTO = 1000000
+				}
+				backoff++
+				i = ack_array[0].index
+				seqn0 = i + 1
+				ack_array = nil
+				exit = 1
+				fmt.Println(ack_array)
 				break
 			}
-		}}
+			if exit == 1 {
+				break
+			}
+		}
+	}
 
 	pc.WriteTo([]byte("FIN"), add)
 	end := time.Now().Sub(startlog)
@@ -374,17 +378,16 @@ func sendFile(file string, pc net.PacketConn, add net.Addr, cp *conn_param) bool
 
 	log_out += "/ " + end.String() + " 999999 0 \n"
 
-	debit := (float32(file_info)*8 / float32(end / time.Millisecond) ) * 1000
+	debit := (float32(file_info) / float32(end/time.Millisecond)) * 1000
 	fmt.Println("Débit is : ", debit, "o/s")
-	fmt.Println(debit/1000000,"Mo/s")
+	fmt.Println(debit/1000000, "Mo/s")
 
 	log_out += "$ " + fmt.Sprintf("%f", debit) + " 999999 0 \n"
 
-
 	var re = regexp.MustCompile(`([.-z]*) ([0-9]+).([0-9]+)µs ([ -Z]*)`)
-  log_out = re.ReplaceAllString(log_out, `$1 0.$2${3}ms $4`)
+	log_out = re.ReplaceAllString(log_out, `$1 0.$2${3}ms $4`)
 	re = regexp.MustCompile(`([ -z]+)ms([ -z]*)`)
-  log_out = re.ReplaceAllString(log_out, `$1 ms$2`)
+	log_out = re.ReplaceAllString(log_out, `$1 ms$2`)
 	f, _ := os.Create("logs/log_send_" + time.Now().String())
 	defer f.Close()
 	f.WriteString(log_out)
@@ -393,8 +396,7 @@ func sendFile(file string, pc net.PacketConn, add net.Addr, cp *conn_param) bool
 	return true
 }
 
-
-func handleClient(add net.Addr, port int, c chan int64){
+func handleClient(add net.Addr, port int, c chan int64) {
 	defer releasePort(port)
 	defer fmt.Println("FIN Transmission")
 	pc, err := net.ListenPacket("udp", "0.0.0.0:"+strconv.Itoa(port))
@@ -404,14 +406,14 @@ func handleClient(add net.Addr, port int, c chan int64){
 	defer pc.Close()
 	var rtt int64
 	select {
-	case rtt =<-c:
+	case rtt = <-c:
 		fmt.Println("ok - RTT", rtt, "ns (1ms = 1 000 000ns)")
 		if rtt == 0 {
 			logs("Deleted")
 			return
 		}
 
-	case <- time.After(10 * time.Second):
+	case <-time.After(10 * time.Second):
 		logs("Deleted")
 		return
 	}
@@ -419,14 +421,14 @@ func handleClient(add net.Addr, port int, c chan int64){
 	for {
 		buffer := make([]byte, 1024)
 		n, _, _ := pc.ReadFrom(buffer)
-		fmt.Println("handle", port, add,"\n"+string(buffer[:n]), n)
-		if sendFile(string(buffer[:n-1]), pc, add, &cp){
+		fmt.Println("handle", port, add, "\n"+string(buffer[:n]), n)
+		if sendFile(string(buffer[:n-1]), pc, add, &cp) {
 			break
 		}
 	}
 }
 
-func main(){
+func main() {
 	args := os.Args[1:]
 	port := "8080"
 	addrWait := make(map[string]waitParam)
@@ -435,9 +437,13 @@ func main(){
 		port = args[0]
 	}
 
+	if len(args) == 2 {
+		debug = args[1] == "true"
+	}
+
 	fmt.Println("Testing ports")
 	portList = make(map[int]void)
-	testPorts(1000,9999)
+	testPorts(1000, 9999)
 	fmt.Println("Initial portList has been set")
 
 	fmt.Println("Launching server")
@@ -457,11 +463,11 @@ func main(){
 		}
 		in := string(buffer)
 		add := addr.String()
-		logs(in ,addr)
+		logs(in, addr)
 		if strings.Contains(in, "SYN") {
-			for k, v := range addrWait{
-				logs(k,time.Now().UnixNano()-  v.toa )
-				if time.Now().UnixNano() - v.toa > 1000000000{
+			for k, v := range addrWait {
+				logs(k, time.Now().UnixNano()-v.toa)
+				if time.Now().UnixNano()-v.toa > 1000000000 {
 					logs("Deleting", k)
 					close(v.c)
 					delete(addrWait, k)
@@ -473,18 +479,18 @@ func main(){
 				go handleClient(addr, p, ch)
 				s := waitParam{
 					toa: time.Now().UnixNano(),
-					c: ch,
+					c:   ch,
 				}
 				addrWait[add] = s
 				pc.WriteTo([]byte("SYN-ACK"+strconv.Itoa(p)), addr)
 			}
 
-		}else if el, found := addrWait[add]; strings.Contains(in, "ACK") && found{
+		} else if el, found := addrWait[add]; strings.Contains(in, "ACK") && found {
 			logs("Got ACK")
 			el.c <- time.Now().UnixNano() - el.toa //RTT dans la goroutine
 			close(el.c)
 			delete(addrWait, add)
-		}else{
+		} else {
 			logs(addrWait)
 			continue
 		}
